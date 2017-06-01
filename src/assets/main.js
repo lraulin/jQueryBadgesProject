e053a14ced135f22c1fbced3ad1f3dcb9ed4f9fd
Lee Raulin <leeraulin@gmail.com>
2017-05-31 20:00:27 -0400

    first commit

--- src/assets/main.js
+++ src/assets/main.js
@@ -1,5 +1,19 @@
 $(function() {
 
-  // your code will go here
+  $.ajax({
+      url: 'https://www.codeschool.com/users/lraulin.json',
+      dataType: 'jsonp',
+      success: function(response) {
+        // handle response
+        console.log(JSON.stringify(response.courses.completed));
+        for (var i = 0; i < response.courses.completed.length; i++) {
+          var title = response.courses.completed[i].title;
+          var url = response.courses.completed[i].url;
+          var badge = response.courses.completed[i].badge;
+          $("#badges").append('<div class="course"><h3>' + title + '</h3><img src="' + badge + '" href="' + url + '"></div>');
+        }
+      }
+    });
+
 
 });
